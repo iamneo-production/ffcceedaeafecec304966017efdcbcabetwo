4d8a6662276b263e197556eaa157d332ec851473
Iamneo <techadmin@iamneo.ai>
2023-09-10 15:17:45 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,383 +1,64 @@
+// Initial game state
+let cells = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9'];
+let currentPlayer = 'X';
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('.btn');
+let conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-function myfunc(){
-    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
-    b1 = document.getElementById("b1").value;
-    b2 = document.getElementById("b2").value;
-    b3 = document.getElementById("b3").value;
-    b4 = document.getElementById("b4").value;
-    b5 = document.getElementById("b5").value;
-    b6 = document.getElementById("b6").value;
-    b7 = document.getElementById("b7").value;
-    b8 = document.getElementById("b8").value;
-    b9 = document.getElementById("b9").value;
+// Function to handle player moves
+const ticTacToe = (element, index) => {
+    // Your game logic here
 
-    var b1btn, b2btn, b3btn, b4btn, b5btn, b6btn, b7btn, b8btn, b9btn;
-    b1btn = document.getElementById("b1");
-    b2btn = document.getElementById("b2");
-    b3btn = document.getElementById("b3");
-    b4btn = document.getElementById("b4");
-    b5btn = document.getElementById("b5");
-    b6btn = document.getElementById("b6");
-    b7btn = document.getElementById("b7");
-    b8btn = document.getElementById("b8");
-    b9btn = document.getElementById("b9");
+    /*
+    **Part 1: Winning Conditions (Add your code here)**
 
-    if((b1 == 'x' || b1 == 'X') && (b2 == 'x' || b2 == 'X') && (b3 == 'x' || b3 == 'X'))    {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b1btn.style.color = "red";
-        b2btn.style.color = "red";
-        b3btn.style.color = "red";
-    }
+    1. Implement the logic to check for winning conditions using the 'conditions' array.
+    2. Display a winning message in the 'result' element when a player wins.
+    3. Disable all buttons after a win.
+    */
 
-    else if((b1 == 'x' || b1 == 'X') && (b4 == 'x' || b4 == 'X') && (b7 == 'x' || b7 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b1btn.style.color = "red";
-        b4btn.style.color = "red";
-        b7btn.style.color = "red";
-    }
+    // Your code to update the game state and check for a win
+    // ...
 
-    else if((b7 == 'x' || b7 == 'X') && (b8 == 'x' || b8 == 'X') && (b9 == 'x' || b9 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        
-        b7btn.style.color = "red";
-        b8btn.style.color = "red";
-        b9btn.style.color = "red";
-    }
+    // Your code to display the current player's turn
+    // ...
 
-    else if((b3 == 'x' || b3 == 'X') && (b6 == 'x' || b6 == 'X') && (b9 == 'x' || b9 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        
-        b3btn.style.color = "red";
-        b6btn.style.color = "red";
-        b9btn.style.color = "red";
-    }
+    // Your code to handle button and cell interactions
+    // ...
+};
 
-    else if((b1 == 'x' || b1 == 'X') && (b5 == 'x' || b5 == 'X') && (b9 == 'x' || b9 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b4btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        
-        b1btn.style.color = "red";
-        b5btn.style.color = "red";
-        b9btn.style.color = "red";
-    }
+    /*
+    **Part 2: Reset Function (Add your code here)**
 
-    else if((b3 == 'x' || b3 == 'X') && (b5 == 'x' || b5 == 'X') && (b7 == 'x' || b7 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b4btn.disabled = true;
-        b6btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b3btn.style.color = "red";
-        b5btn.style.color = "red";
-        b7btn.style.color = "red";
-    }
+    1. Implement a new function that resets the game to its initial state.
+    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
+    3. Update the 'result' element to indicate the current player's turn.
+    4. Re-enable all buttons for a new game.
+    */
 
-    else if((b2 == 'x' || b2 == 'X') && (b5 == 'x' || b5 == 'X') && (b8 == 'x' || b8 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b4btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b2btn.style.color = "red";
-        b5btn.style.color = "red";
-        b8btn.style.color = "red";
-    }
+// Function to reset the game
+const resetGame = () => {
+    // Your code to reset the game state
+    // ...
 
-    else if((b4 == 'x' || b4 == 'X') && (b5 == 'x' || b5 == 'X') && (b6 == 'x' || b6 == 'X'))   {
-        document.getElementById('result-container').innerHTML = "Player X won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b4btn.style.color = "red";
-        b5btn.style.color = "red";
-        b6btn.style.color = "red";
-    }
+    // Your code to update the 'result' element
+    // ...
 
-    else if((b1 == 'o' || b1 == 'O') && (b2 == 'o' || b2 == 'O') && (b3 == 'o' || b3 == 'O'))    {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b1btn.style.color = "red";
-        b2btn.style.color = "red";
-        b3btn.style.color = "red";
-    }
+    // Your code to re-enable buttons
+    // ...
+};
 
-    else if((b1 == 'o' || b1 == 'O') && (b4 == 'o' || b4 == 'O') && (b7 == 'o' || b7 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b1btn.style.color = "red";
-        b4btn.style.color = "red";
-        b7btn.style.color = "red";
-    }
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => ticTacToe(btn, i));
+});
 
-    else if((b7 == 'o' || b7 == 'O') && (b8 == 'o' || b8 == 'O') && (b9 == 'o' || b9 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        
-        b7btn.style.color = "red";
-        b8btn.style.color = "red";
-        b9btn.style.color = "red";
-    }
-
-    else if((b3 == 'o' || b3 == 'O') && (b6 == 'o' || b6 == 'O') && (b9 == 'o' || b9 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        
-        b3btn.style.color = "red";
-        b6btn.style.color = "red";
-        b9btn.style.color = "red";
-    }
-
-    else if((b1 == 'o' || b1 == 'O') && (b5 == 'o' || b5 == 'O') && (b9 == 'o' || b9 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b4btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        
-        b1btn.style.color = "red";
-        b5btn.style.color = "red";
-        b9btn.style.color = "red";
-    }
-
-    else if((b3 == 'o' || b3 == 'O') && (b5 == 'o' || b5 == 'O') && (b7 == 'o' || b7 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b4btn.disabled = true;
-        b6btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b3btn.style.color = "red";
-        b5btn.style.color = "red";
-        b7btn.style.color = "red";
-    }
-
-    else if((b2 == 'o' || b2 == 'O') && (b5 == 'o' || b5 == 'O') && (b8 == 'o' || b8 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b4btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b2btn.style.color = "red";
-        b5btn.style.color = "red";
-        b8btn.style.color = "red";
-    }
-
-    else if((b4 == 'o' || b4 == 'O') && (b5 == 'o' || b5 == 'O') && (b6 == 'o' || b6 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Player O won";
-        b1btn.disabled = true;
-        b2btn.disabled = true;
-        b3btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-        
-        b4btn.style.color = "red";
-        b5btn.style.color = "red";
-        b6btn.style.color = "red";
-    }
-
-    else if((b1 == 'X' || b1 == 'O') &&
-    (b2 == 'X' || b2 == 'O') && 
-    (b3 == 'X' || b3 == 'O') && 
-    (b4 == 'X' || b4 == 'O') && 
-    (b5 == 'X' || b5 == 'O') && 
-    (b6 == 'X' || b6 == 'O') && 
-    (b7 == 'X' || b7 == 'O') && 
-    (b8 == 'X' || b8 == 'O') && 
-    (b9 == 'X' || b9 == 'O'))   {
-        document.getElementById('result-container').innerHTML = "Match Tie";
-    }
-
-    else{
-        if(flagg == 1){
-            document.getElementById('result-container').innerHTML = "Player X Turn";
-        }
-        else{
-            document.getElementById('result-container').innerHTML = "Player O Turn";
-        }
-    }
-}
-
-function myfunc_2(){
-    location.reload();
-    b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = '';
-}
-
-flag = 1;
-function myfunc_3(){
-    if(flag == 1){
-        document.getElementById("b1").value = "X";
-        document.getElementById("b1").disabled = true;
-    }
-    else{
-        document.getElementById("b1").value = "O";
-        document.getElementById("b1").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_4(){
-    if(flag == 1){
-        document.getElementById("b2").value = "X";
-        document.getElementById("b2").disabled = true;
-    }
-    else{
-        document.getElementById("b2").value = "O";
-        document.getElementById("b2").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_5(){
-    if(flag == 1){
-        document.getElementById("b3").value = "X";
-        document.getElementById("b3").disabled = true;
-    }
-    else{
-        document.getElementById("b3").value = "O";
-        document.getElementById("b3").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_6(){
-    if(flag == 1){
-        document.getElementById("b4").value = "X";
-        document.getElementById("b4").disabled = true;
-    }
-    else{
-        document.getElementById("b4").value = "O";
-        document.getElementById("b4").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_7(){
-    if(flag == 1){
-        document.getElementById("b5").value = "X";
-        document.getElementById("b5").disabled = true;
-    }
-    else{
-        document.getElementById("b5").value = "O";
-        document.getElementById("b5").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_8(){
-    if(flag == 1){
-        document.getElementById("b6").value = "X";
-        document.getElementById("b6").disabled = true;
-    }
-    else{
-        document.getElementById("b6").value = "O";
-        document.getElementById("b6").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_9(){
-    if(flag == 1){
-        document.getElementById("b7").value = "X";
-        document.getElementById("b7").disabled = true;
-    }
-    else{
-        document.getElementById("b7").value = "O";
-        document.getElementById("b7").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_10(){
-    if(flag == 1){
-        document.getElementById("b8").value = "X";
-        document.getElementById("b8").disabled = true;
-    }
-    else{
-        document.getElementById("b8").value = "O";
-        document.getElementById("b8").disabled = true;
-        flag = 1;
-    }
-}
-
-function myfunc_11(){
-    if(flag == 1){
-        document.getElementById("b9").value = "X";
-        document.getElementById("b9").disabled = true;
-    }
-    else{
-        document.getElementById("b9").value = "O";
-        document.getElementById("b9").disabled = true;
-        flag = 1;
-    }
-}
+document.querySelector('#reset').addEventListener('click', resetGame);
